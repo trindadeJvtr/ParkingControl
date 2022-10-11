e05fc7683746fe2a371d358c61bf875a17f31333
trindade <joaovtr.trindade@gmail.com>
2022-10-10 23:02:12 -0300

    criação de dtos e criação parcial de metodos controller

---
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -0,0 +1,14 @@
+package com.api.parkingcontrol.service;
+
+import com.api.parkingcontrol.repositorie.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+}
