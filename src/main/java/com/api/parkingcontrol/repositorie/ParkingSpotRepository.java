00cd9214ac617f55e1a54fee0a7b27aaf91db8af
trindade <joaovtr.trindade@gmail.com>
2022-10-12 00:57:20 -0300

    adição de metodo post com verificacoes e teste no postman

--- src/main/java/com/api/parkingcontrol/repositorie/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositorie/ParkingSpotRepository.java
@@ -9,6 +9,8 @@ import java.util.UUID;
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
 
-
+boolean existsByLicensePlateCar(String licensePlateCar);
+boolean existsByParkingSpotNumber(String licensePlateCar);
+boolean existsByApartmentAndBlock(String licensePlateCar);
 
 }
--- src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -5,6 +5,8 @@ import com.api.parkingcontrol.repositorie.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class ParkingSpotService {
     final ParkingSpotRepository parkingSpotRepository;
@@ -13,7 +15,15 @@ public class ParkingSpotService {
         this.parkingSpotRepository = parkingSpotRepository;
     }
 
+    @Transactional
     public Object save(ParkingSpotModel parkingSpotModel) {
-        return "";
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return existsByLicensePlateCar(licensePlateCar);
+    }
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return existsByLicensePlateCar(parkingSpotNumber);
     }
 }
